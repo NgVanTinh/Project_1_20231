1a8a4b4ebfc60c15ed14e07b8fdb8c097390317a
Zinne0811 <nguyen.tinh.8113@gmail.com>
2024-01-07 13:18:23 +0700

    them mot so do hoa chia ca cua so do thi

--- main.cpp
+++ main.cpp
@@ -4,10 +4,11 @@
 #include <conio.h>
 #include <graphics.h>
 #include <math.h>
-#define WIDTH 900
-#define HEIGHT 630
+#define WIDTH 1000
+#define HEIGHT 646
 #define WINDOW_SIZE 512
 #define MAX_K 256
+#define DISTANCE_HOLD 10
 
 typedef struct header {
     char chunk_id[4];
@@ -27,25 +28,9 @@ typedef struct header {
 
 typedef struct header* header_p;
 
-void drawWaveform(int16_t *data, int num_samples, int max_amplitude, int color, int startY) {
-//	k/c giua cac mau
-	
-    float sample_spacing = (float)getmaxx() /  num_samples;
-
-    for ( int i = 0; i < num_samples ; ++i) {
-        int x1 = i * sample_spacing; 
-        int y1 = startY - (data[i] * 100 / max_amplitude); 
-        int x2 = (i + 1) * sample_spacing; 
-        int y2 = startY - (data[i + 1] * 100 / max_amplitude); 
-
-        setcolor(color);
-        line(x1, y1, x2, y2);
-    }
-}
-
 // reset cua so ve ham tu tuong quan
 void resetDrawAutoCorrelation(int left, int top, int right, int bottom){
-	// Ðat màu vi?n hình ch? nh?t (ví d?: tr?ng)
+	
     setcolor(BLACK);
 
     // V? vi?n hình ch? nh?t
@@ -57,20 +42,69 @@ void resetDrawAutoCorrelation(int left, int top, int right, int bottom){
     // Tô màu n?n cho hình ch? nh?t
     // S? d?ng bar d? t?o hình ch? nh?t d?c màu
     bar(left + 1, top + 1, right, bottom);
+    
+    
+    setcolor(YELLOW);
+	line(left,333,right,333);
+    
+}
+
+void drawFillRectangle(int left, int top, int right, int bottom, int color){
+	// Ðat màu vi?n hình ch? nh?t (ví d?: tr?ng)
+    setcolor(color);
+
+    // V? vi?n hình ch? nh?t
+    rectangle(left, top, right, bottom);
+
+    // Ð?t màu n?n và ki?u tô màu
+    setfillstyle(SOLID_FILL, color);
+
+    // Tô màu n?n cho hình ch? nh?t
+    // S? d?ng bar d? t?o hình ch? nh?t d?c màu
+    bar(left + 1, top + 1, right, bottom);
+}
+
+// ve gia dien
+void drawInterface(){
+	
+    drawFillRectangle(0,0,30,646,LIGHTGRAY);
+    drawFillRectangle(971,0,1000,646,LIGHTGRAY);
+    
+    drawFillRectangle(30,0,970,10,LIGHTGRAY);
+    drawFillRectangle(30,212,970,222,LIGHTGRAY);
+    drawFillRectangle(30,424,970,434,LIGHTGRAY);
+    drawFillRectangle(30,636,970,646,LIGHTGRAY);
+    
+    drawFillRectangle(491,222,510,424,LIGHTGRAY);
+}
+
+void drawWaveform(int16_t *data, int num_samples, int max_amplitude, int color, int startY) {
+//	k/c giua cac mau
+	
+    float sample_spacing = (float)940 /  num_samples;
+
+    for ( int i = 0; i < num_samples ; ++i) {
+        int x1 = 31 + i * sample_spacing; 
+        int y1 = startY - (data[i] * 100 / max_amplitude); 
+        int x2 = 31 + (i + 1) * sample_spacing; 
+        int y2 = startY - (data[i + 1] * 100 / max_amplitude); 
+
+        setcolor(color);
+        line(x1, y1, x2, y2);
+    }
 }
 
 // ve duong doc o wave form tro de vi tri am thanh dang phan tich ham R(k);
-void drawLineInWaveForm(int start, int num_samples, int color){
+void drawLineInWaveForm(int start, float sample_spacing, int color){
 	static bool isDrawn = false;
     static int *buffer = NULL;
 	
-	float sample_spacing = (float)getmaxx() /  num_samples;
-    int x1 = start * sample_spacing; 
-    int y1 = 0;
-    int x2 = (start + 1) * sample_spacing; 
-    int y2 = 210; 
+    int x1 = 30 + start * sample_spacing; 
+    int y1 = 10;
+    int x2 = 30 + (start + WINDOW_SIZE) * sample_spacing; 
+    int y2 = 212; 
+
     if (!isDrawn) {
-        // L?n d?u tiên hàm du?c g?i, luu và v? hình ch? nh?t
         int size = imagesize(x1, y1, x2, y2);
         buffer = (int*) malloc(size);
         getimage(x1, y1, x2, y2, buffer);
@@ -78,7 +112,7 @@ void drawLineInWaveForm(int start, int num_samples, int color){
         rectangle(x1, y1, x2, y2);
         isDrawn = true;
     } else {
-        // Khi hàm du?c g?i l?n th? hai, xóa hình ch? nh?t
+        // Khi hàm goi lan thu 2 thi se xoa hinh chu nhat
         putimage(x1, y1, buffer, COPY_PUT);
         free(buffer);
         buffer = NULL;
@@ -87,16 +121,17 @@ void drawLineInWaveForm(int start, int num_samples, int color){
 
 }
 
-// truyen vao du lieu, vi tri mau bat dau cua mau, so mau, mang chua gia tri ham R(k), mau, toa do y bat dau ve(giong nhau cua ca 2 do thi) con toa do x mac dinh la 0
+// truyen vao du lieu, vi tri mau bat dau cua mau, so mau, mang chua gia tri ham R(k), mau, 
+//toa do y bat dau ve(giong nhau cua ca 2 do thi) con toa do x mac dinh la 0
 void calculateAndDrawAutoCorrelationAndWWaveForm(int16_t *data, int start, int window_size, float *autocorr, int color, int startY) {
 	
-	// ve do thi song
-	float sample_spacing = (float)450.0 /  window_size;
+	// kc giua cac mau la giong nhau o ca 2 do thi
+	float sample_spacing = (float)460.0 /  window_size;
 
     for ( int i = 0; i < window_size ; ++i) {
-        int x1 = i * sample_spacing; 
+        int x1 = 30 + i * sample_spacing; 
         int y1 = startY - (data[i + start] * 100 / 32767); 
-        int x2 = (i + 1) * sample_spacing; 
+        int x2 = 30 + (i + 1) * sample_spacing; 
         int y2 = startY - (data[i + start + 1] * 100 / 32767); 
 
         setcolor(color);
@@ -116,28 +151,26 @@ void calculateAndDrawAutoCorrelationAndWWaveForm(int16_t *data, int start, int w
         autocorr[delay] /= (window_size - delay); // Chia trung bình
     }
 
-    // Tìm giá tri max
+    // Tìm giá tri max cua ham tu tuong quang trong doan 512 mau dang xet
 	
-	float max_val = autocorr[0]; 
+	float max_val = fabs(autocorr[0]); 
 	for (int i = 1; i < window_size; ++i) {
-	    if (autocorr[i] > max_val) {
-	        max_val = autocorr[i];
+	    if (fabs(autocorr[i])> max_val) {
+	        max_val = fabs(autocorr[i]);
 	    }
 	}
 
 
-    // V? d? th? t? tuong quan
+    // ve do thi ham tu tuong quan
     for (int i = 0; i < window_size - 1; ++i) {
-        int y1 = startY - (autocorr[i] / max_val) * 100; 
-        int y2 = startY - (autocorr[i+1] / max_val) * 100; 
-        int x1 = 500 + i * sample_spacing;
-        int x2 = 500 + (i + 1) * sample_spacing;
+        int y1 = startY -( (autocorr[i] / max_val) * 80 ); 
+        int y2 = startY -( (autocorr[i+1] / max_val) * 80 ); 
+        int x1 = 511 + i * sample_spacing;
+        int x2 = 511 + (i + 1) * sample_spacing;
 
         setcolor(color);
         line(x1, y1, x2, y2);
     }
-    
-    // ve hinh chu nhat tro den vi tri 512 mau am thanh dang xet ham tu tuong quan
     
 
 }
@@ -148,11 +181,21 @@ void drawAndClearAutoCorrelation(int16_t *data, int num_samples, int color, int 
     float autocorr[window_size];
     int start;
 
-    for (start = 0; start <= num_samples - window_size; start += window_size) {
-    	int i = 0;
-		resetDrawAutoCorrelation(0,200,1000,440);
-        calculateAndDrawAutoCorrelationAndWWaveForm(data, start, window_size, autocorr, color, startY );
-        delay(300); 
+    for (start = 0; start <= num_samples - window_size; start += window_size/2) {
+    	
+    	float sample_spacing = (float)970 /  num_samples;
+        if(start + window_size/2 >= num_samples){
+        	drawLineInWaveForm(start,sample_spacing,RED);
+        	calculateAndDrawAutoCorrelationAndWWaveForm(data, start, window_size, autocorr, color, startY );
+		}
+		else {
+			resetDrawAutoCorrelation(30,223,490,415);
+			resetDrawAutoCorrelation(511,223,970,416);
+			drawLineInWaveForm(start,sample_spacing,RED);
+	        calculateAndDrawAutoCorrelationAndWWaveForm(data, start, window_size, autocorr, color, startY );
+	        delay(300); 
+	        drawLineInWaveForm(start,sample_spacing,RED);	
+		}
     }
 }
 
@@ -191,17 +234,16 @@ int main() {
 	// do dai thoi gian cua file
 	float timeLength = (meta->subchunk2_size * 8.0)/(meta->sample_rate * meta->num_channels * meta->bits_per_sample);
     
-    
-//    int gd = DETECT, gm;
-//    initgraph(&gd, &gm, NULL);
+    // Ve cac do thi
     int width = WIDTH;
     int height = HEIGHT;
     initwindow(width, height);
-	drawWaveform(inbuff16, SampleNumber,32767, GREEN, getmaxy() / 6);
+    
+    drawInterface();
+    
+	drawWaveform(inbuff16, SampleNumber,32767, GREEN, 111);
 	
-	for (int start = 0; start < SampleNumber - WINDOW_SIZE; start += WINDOW_SIZE/2) {
-	    drawAndClearAutoCorrelation(inbuff16, SampleNumber, GREEN, getmaxy() / 2 );
-	}
+	drawAndClearAutoCorrelation(inbuff16, SampleNumber, GREEN, 333 );
 
     getch();
 
